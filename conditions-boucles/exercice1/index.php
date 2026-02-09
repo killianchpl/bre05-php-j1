edc40eb1713a0ea0d1ca24f85653065b2f4e472a
Killian Chapelain <killian.chapelain@outlook.com>
2026-02-09 13:13:06 +0000

    condition boucle exo fin

---
+++ conditions-boucles/exercice1/index.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$animals = ["Chat", "Chien", "Lapin", "Souris"];
+
+for ( $i = 0; $i < count($animals); $i++ ) {
+    
+    echo $animals[$i] . "<br>"; 
+    
+}
+
+?>
---
+++ conditions-boucles/exercice3/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$user = [
+    "name" => "James",
+    "age" => 28
+];
+
+
+foreach ($user as $key => $value) {
+    
+    echo "My $key is $value. <br>";
+    
+}
+
+?>
